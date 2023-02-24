a0d0e97496ac4c55cb09542a52b9ca5ce800b8c5
Johan Schoultz <jkmschoultz@gmail.com>
2023-02-24 20:26:59 +0000

    Added recycler view

--- app/src/main/java/com/app/repotracker/MainActivity.java
+++ app/src/main/java/com/app/repotracker/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.AsyncTaskLoader;
 import androidx.loader.content.Loader;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -21,6 +23,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.SearchView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.app.repotracker.utilities.NetworkUtils;
 
@@ -28,7 +31,7 @@ import java.io.IOException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements
-        LoaderManager.LoaderCallbacks<String> {
+        LoaderManager.LoaderCallbacks<String>, Adapter.ListItemClickListener {
 
     private static final String SEARCH_QUERY_URL_EXTRA = "query";
     private static final int GITHUB_SEARCH_LOADER = 22;
@@ -37,6 +40,22 @@ public class MainActivity extends AppCompatActivity implements
     private TextView errorMessage;
     private ProgressBar loadingSymbol;
     private SearchView search;
+
+    private static final int NUM_LIST_ITEMS = 5;
+
+    /*
+     * References to RecyclerView and Adapter to reset the list to its
+     * "pretty" state when the reset menu item is clicked.
+     */
+    private Adapter adapter;
+    private RecyclerView recyclerView;
+    // Create a Toast variable called mToast to store the current Toast
+    /*
+     * If we hold a reference to our Toast, we can cancel it (if it's showing)
+     * to display a new Toast. If we didn't do this, Toasts would be delayed
+     * in showing up if you clicked many list items in quick succession.
+     */
+    private Toast mToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +66,33 @@ public class MainActivity extends AppCompatActivity implements
         searchResults = (TextView) findViewById(R.id.searchResults);
         errorMessage = (TextView) findViewById(R.id.error_message);
         loadingSymbol = (ProgressBar) findViewById(R.id.loading_symbol);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+
+        /*
+         * A LinearLayoutManager is responsible for measuring and positioning item views within a
+         * RecyclerView into a linear list. This means that it can produce either a horizontal or
+         * vertical list depending on which parameter you pass in to the LinearLayoutManager
+         * constructor. By default, if you don't specify an orientation, you get a vertical list.
+         * In our case, we want a vertical list, so we don't need to pass in an orientation flag to
+         * the LinearLayoutManager constructor.
+         *
+         * There are other LayoutManagers available to display your data in uniform grids,
+         * staggered grids, and more! See the developer documentation for more details.
+         */
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        /*
+         * Use this setting to improve performance if you know that changes in content do not
+         * change the child layout size in the RecyclerView
+         */
+        recyclerView.setHasFixedSize(true);
+        //  Pass in this as the ListItemClickListener to the GreenAdapter constructor
+        /*
+         * The GreenAdapter is responsible for displaying each item in the list.
+         */
+        adapter = new Adapter(NUM_LIST_ITEMS, this);
+        recyclerView.setAdapter(adapter);
 
         if (savedInstanceState != null) {
             String queryUrl = savedInstanceState.getString(SEARCH_QUERY_URL_EXTRA);
@@ -62,7 +108,6 @@ public class MainActivity extends AppCompatActivity implements
      */
     private void makeGithubSearchQuery() {
         String githubQuery = search.getQuery().toString();
-        System.out.println(githubQuery);
 
         // If search is empty, display empty search message
         if (TextUtils.isEmpty(githubQuery)) {
@@ -201,10 +246,40 @@ public class MainActivity extends AppCompatActivity implements
                 return true;
 
             case R.id.reset:
-                // do something
+                // Pass in this as the ListItemClickListener to the GreenAdapter constructor
+                adapter = new Adapter(NUM_LIST_ITEMS, this);
+                recyclerView.setAdapter(adapter);
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListItemClick(int clickedItemIndex) {
+        // In the beginning of the method, cancel the Toast if it isn't null
+        /*
+         * Even if a Toast isn't showing, it's okay to cancel it. Doing so
+         * ensures that our new Toast will show immediately, rather than
+         * being delayed while other pending Toasts are shown.
+         *
+         * Comment out these three lines, run the app, and click on a bunch of
+         * different items if you're not sure what I'm talking about.
+         */
+        if (mToast != null) {
+            mToast.cancel();
+        }
+
+        // Show a Toast when an item is clicked, displaying that item number that was clicked
+        /*
+         * Create a Toast and store it in our Toast field.
+         * The Toast that shows up will have a message similar to the following:
+         *
+         *                     Item #42 clicked.
+         */
+        String toastMessage = "Item #" + clickedItemIndex + " clicked.";
+        mToast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+
+        mToast.show();
     }
 
     @Override
